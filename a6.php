5c005752558e57f0279fb7f996a0440499654800
Shreevatsatg <tgshreevatsa@gmail.com>
2026-05-26 22:25:54 +0530

    indentation fix

--- a6.php
+++ a6.php
@@ -2,15 +2,18 @@
     <form method="POST"> 
         Enter a word: <input type="text" name="word"> 
         <input type="submit" name="submit"> 
-    </form>     <?php     if (isset($_POST["submit"])) { 
+    </form>     
+    <?php     
+    if (isset($_POST["submit"])) { 
         $dict = array( 
             "apple" => "it is a fruit", 
             "book" => "used to gain info", 
-            "mobile" => "used to comunicate" 
+            "mobile" => "used to communicate" 
         ); 
         $word = strtolower($_POST["word"]);         
         if (array_key_exists($word, $dict))             
             echo "<b>Meaning</b>: $dict[$word]";         
         else              
             echo "$word not found in dictionary"; 
-    }     ?> 
+        }     
+    ?> 
